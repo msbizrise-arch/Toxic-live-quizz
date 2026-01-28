418458676d186dcc8fd01990948fe9a03a30da77
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:37:47 +0530

    Update quiz.controller.js

--- server/controllers/quiz.controller.js
+++ server/controllers/quiz.controller.js
@@ -1,1 +1,18 @@
+const Quiz = require('../models/Quiz');
+const { extractQuestions } = require('./pdfQuestion.controller');
 
+exports.createQuiz = async (req, res) => {
+  const { testName, pdfName } = req.body;
+
+  const questions = await extractQuestions(`uploads/${pdfName}`);
+
+  const quiz = await Quiz.create({
+    testName,
+    questions,
+    timeLimit: questions.length * 30 // 30 sec per question
+  });
+
+  res.json({
+    link: `https://resonance-live-stream-xmtr.onrender.com/${testName}`
+  });
+};
