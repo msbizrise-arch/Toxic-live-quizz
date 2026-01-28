65d36a3bf4aff93c0a243dfb5a303bc8569f651b
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:28:18 +0530

    Update upload.js

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,1 +1,12 @@
+const input = document.getElementById('pdf');
+input.onchange = () => {
+  const form = new FormData();
+  form.append('pdf', input.files[0]);
 
+  const xhr = new XMLHttpRequest();
+  xhr.upload.onprogress = e => {
+    document.getElementById('progress').innerText = Math.round((e.loaded/e.total)*100) + '%';
+  };
+  xhr.open('POST', '/api/upload');
+  xhr.send(form);
+};
