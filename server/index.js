5af197322c559dede0cce5b7f719132aee740a4f
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:30:58 +0530

    Set up server with socket initialization

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
+require('dotenv').config();
+const app = require('./app');
+const http = require('http');
+const { initSocket } = require('./socket');
 
+const server = http.createServer(app);
+initSocket(server);
+
+server.listen(process.env.PORT || 10000, () => {
+  console.log('Server running');
+});
