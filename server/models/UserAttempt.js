ffaf20b5358a47eb4fc3864cf18bfbef72565fc6
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:32:04 +0530

    Update UserAttempt.js

--- server/models/UserAttempt.js
+++ server/models/UserAttempt.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const AttemptSchema = new mongoose.Schema({
+  username: String,
+  testName: String,
+  score: Number,
+  attemptCount: Number,
+  submittedAt: Date
+});
+
+module.exports = mongoose.model('UserAttempt', AttemptSchema);
