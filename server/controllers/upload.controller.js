71dfaf1c3a6b7c949929544ea2e2b62b2ffeda67
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:49:47 +0530

    Update upload.controller.js

--- server/controllers/upload.controller.js
+++ server/controllers/upload.controller.js
@@ -1,1 +1,5 @@
-
+// server/controllers/upload.controller.js
+exports.uploadPDF = (req, res) => {
+    if (!req.file) return res.status(400).json({ success: false, message: "No file uploaded" });
+    res.json({ success: true, filename: req.file.filename });
+};
