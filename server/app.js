1b7b7d36d4900fb9f6141d8e3a2a38d2f675dac0
msbizrise-arch <msbizrise@gmail.com>
2026-01-28 13:31:21 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
+const uploadRoutes = require('./routes/upload.routes');
+const quizRoutes = require('./routes/quiz.routes');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api/upload', uploadRoutes);
+app.use('/api/quiz', quizRoutes);
+
+app.get('/:testName', (req, res) => {
+  res.sendFile(__dirname + '/../public/quiz.html');
+});
+
+module.exports = app;
